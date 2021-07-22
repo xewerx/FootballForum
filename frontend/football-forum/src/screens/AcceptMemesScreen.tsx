edb69919f1f1fc7daca7dd918da195a63115a30b
Ewaryst Ławecki <ewaryst@spoko.pl>
2021-07-22 13:01:13 +0200

    contribution test

--- frontend/football-forum/src/screens/AcceptMemesScreen.tsx
+++ frontend/football-forum/src/screens/AcceptMemesScreen.tsx
@@ -24,6 +24,7 @@ const AcceptMemesScreen: React.FC<IProps> = (props) => {
         <div>
             <h1>Akceptuj memy</h1>
             <p>Tutaj jako admin akceptujesz memy dodane przez innych użytkowników forum. Przed akceptacją nie pojawią się one na głównej stronie.</p>
+            
             <MemesContainer isAcceptationMemes={true} ></MemesContainer>
         </div>
     )
